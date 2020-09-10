4d67d1c630bf8dcaafa8e6d49c292b523524366c
SameerAhmad125 <70534521+SameerAhmad125@users.noreply.github.com>
2020-09-10 19:00:31 +0500

    CUBE

---
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	
+	void Update () 
+	{
+		if(Input.anyKey)
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}	
+	}
+}
---
+++ New Unity Project/Assets/Scripts/folloCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class folloCube : MonoBehaviour {
+	public Transform player;
+	public Vector3 cameraPosition;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = player.position + cameraPosition;
+	}
+}
